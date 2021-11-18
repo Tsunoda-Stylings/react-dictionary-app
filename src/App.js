7a839df86eeaaf622a2ca952b8f42ad973653b59
Jasmine Tsunoda <jazzyfizzle423@gmail.com>
2021-11-18 20:56:33 +0900

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
@@ -5,6 +6,10 @@ function App() {
     <div className="App">
       <div className="container">
         <h1 className="title">What would you like to know?</h1>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="footer">Coded by Jasmine Tsunoda</footer>
       </div>
     </div>
   );
