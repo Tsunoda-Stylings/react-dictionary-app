1480a9e78361df63fe69cc095628d2acb227856c
Jasmine Tsunoda <jazzyfizzle423@gmail.com>
2021-11-18 19:01:43 +0900

    changed CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import background from "./logo.jpg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <div
-          style={{
-            backgroundImage: `url(${background})`,
-            backgroundPosition: "center",
-            backgroundSize: "contain",
-            backgroundRepeat: "no-repeat",
-            width: "100%",
-            height: "750px",
-          }}
-        >
           <h1 className="hero-title">Dictionary</h1>
         </div>
       </div>
-    </div>
   );
 }
 
