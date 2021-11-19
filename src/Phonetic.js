9a76aa34a0e83e711741fc459f2f71ad29e14df9
Jasmine Tsunoda <jazzyfizzle423@gmail.com>
2021-11-19 13:33:52 +0900

    Update Phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,12 +5,7 @@ import { faPlay } from "@fortawesome/free-solid-svg-icons";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a
-        href={props.phonetic.audio}
-        target="_blank"
-        rel="noreferrer"
-        rel="noopener"
-      >
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         <FontAwesomeIcon icon={faPlay} />
       </a>{" "}
       {""}
