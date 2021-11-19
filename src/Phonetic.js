aca46650a51d3cf9bc4bed71d13cabc2a8da0dc1
Jasmine Tsunoda <jazzyfizzle423@gmail.com>
2021-11-19 13:25:16 +0900

    added phonetics component

---
+++ src/Phonetic.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank">
+        <FontAwesomeIcon icon={faPlay} />
+      </a>{" "}
+      {""}
+      {props.phonetic.text}
+    </div>
+  );
+}
